4a56d18f536c0e707d5751c55d46852d4bf7b3be
yalegama <nadunyalegama@gmail.com>
2021-08-13 10:50:55 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const quizData=[
+    {
+        question: 'Answer 2+2=',
+        a: '1',
+        b: '2',
+        c: '3',
+        d: '4',
+        correct:'d',
+    },
+    {
+        question: 'What Is The Best Programming Language In World',
+        a: 'java',
+        b: 'python',
+        c: 'dart',
+        d: 'javascript',
+        correct:'d',
+    },
+    {
+        question: 'Answer 1+2=',
+        a: '1',
+        b: '2',
+        c: '3',
+        d: '4',
+        correct:'c',
+    }
+]
