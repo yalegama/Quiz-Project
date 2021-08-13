6a49ce92a4c3d022683d3e98e5e035a7f7e5e2b3
yalegama <nadunyalegama@gmail.com>
2021-08-13 17:32:33 +0530

    third commit

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const quizData=[
     {
-        question: 'Answer 2+2=',
-        a: '1',
-        b: '2',
-        c: '3',
-        d: '4',
-        correct:'d',
+        question: 'What is the national sport in Sri Lanka',
+        a: 'Cricket',
+        b: 'Vollyball',
+        c: 'Football',
+        d: 'Batminton',
+        correct:'b',
     },
     {
-        question: 'What Is The Best Programming Language In World',
-        a: 'java',
-        b: 'python',
-        c: 'dart',
-        d: 'javascript',
-        correct:'d',
+        question: 'Who is the priministar of Sri Lanka',
+        a: 'Mahinda Rajapaksa',
+        b: 'Ranil Wickramasinghe',
+        c: 'Sajith Premadasa',
+        d: 'Anura Kumara Dissanayake',
+        correct:'a',
     },
     {
         question: 'Answer 1+2=',
@@ -24,3 +24,26 @@ const quizData=[
         correct:'c',
     }
 ]
+const questionText=document.getElementById('question');
+const answer_a=document.getElementById('text_a');
+const answer_b=document.getElementById('text_b');
+const answer_c=document.getElementById('text_c');
+const answer_d=document.getElementById('text_d');
+
+loadQuiz();
+
+let currentQuiz= 0;
+
+function loadQuiz(){
+    currentQuizData=quizData(currentQuiz);
+
+    questionText=currentQuizData.question;
+    answer_a=currentQuizData.a;
+    answer_b=currentQuizData.b;
+    answer_c=currentQuizData.c;
+    answer_d=currentQuizData.d;
+
+}
+
+console.log(loadQuiz);
+
