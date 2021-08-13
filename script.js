cca3aaefdaba9099775fb9ee48e985434ce8fe77
yalegama <nadunyalegama@gmail.com>
2021-08-13 20:05:18 +0530

    fifth commit

--- script.js
+++ script.js
@@ -1,50 +1,52 @@
-const quizData=[
+quizData=[
     {
-        question: 'What is the national sport in Sri Lanka',
-        a: 'Cricket',
-        b: 'Vollyball',
-        c: 'Football',
-        d: 'Batminton',
-        correct:'b',
+        question: 'What is the capital of Sri Lanka',
+        a: 'kandy',
+        b: 'Sri Jayawardanapura Kotte',
+        c: 'Colombo',
+        d: 'Galle',
+        answer: 'b'
     },
     {
         question: 'Who is the priministar of Sri Lanka',
         a: 'Mahinda Rajapaksa',
         b: 'Ranil Wickramasinghe',
-        c: 'Sajith Premadasa',
-        d: 'Anura Kumara Dissanayake',
-        correct:'a',
+        c: 'sajith Premadasa',
+        d: 'Anurakumara Dissanayake',
+        answer: 'a'
     },
     {
-        question: 'Answer 1+2=',
-        a: '1',
-        b: '2',
-        c: '3',
-        d: '4',
-        correct:'c',
+        question: 'What is the national sport in Sri Lanka',
+        a: 'FootBall',
+        b: 'Cricket',
+        c: 'VollyBall',
+        d: 'Batminton',
+        answer: 'c'
     }
 ]
-const questionText=document.getElementById('question');
-const answer_a=document.getElementById('text_a');
-const answer_b=document.getElementById('text_b');
-const answer_c=document.getElementById('text_c');
-const answer_d=document.getElementById('text_d');
 
+var questions=document.getElementById('question');
+var answer_a=document.getElementById('text_a');
+var answer_b=document.getElementById('text_b');
+var answer_c=document.getElementById('text_c');
+var answer_d=document.getElementById('text_d');
+var submitButton=document.getElementById('button');
 
+let currentQuestion=0;
 
-let currentQuiz= 0;
+loadQuestion();
+function loadQuestion(){
+    const currentQuestionData=quizData[currentQuestion];
 
-loadQuiz();
-
-function loadQuiz(){
-   const currentQuizData=quizData[currentQuiz];
-
-    questionText.innerText=currentQuizData.question;
-    answer_a.innerText=currentQuizData.a;
-    answer_b.innerText=currentQuizData.b;
-    answer_c.innerText=currentQuizData.c;
-    answer_d.innerText=currentQuizData.d;
+    questions.innerText=currentQuestionData.question;
+    answer_a.innerText=currentQuestionData.a;
+    answer_b.innerText=currentQuestionData.b;
+    answer_c.innerText=currentQuestionData.c;
+    answer_d.innerText=currentQuestionData.d;
+    currentQuestion++;
 
 }
-
-
+submitButton.addEventListener('click', ()=>{
+    currentQuestion++;
+    loadQuestion();
+})
